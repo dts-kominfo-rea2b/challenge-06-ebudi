a563769859a3e934cc9d070d4fb51d714b32b7ca
Eko Budiyanto <eko.bye@gmail.com>
2022-06-12 20:14:35 +0700

    Challenge-Advanced CallBack

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,41 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  const tampilData = [];
+
+  //bacaData file1
+  fs.readFile(file1, "utf-8", (err, data) => {
+    if (err) {
+      return console.log(`error : ${err}`);
+    }
+    let konten = JSON.parse(data);
+    pesan = konten.message;
+    tampilData[0] = pesan.split(" ")[1];
+
+    //bacaData file2
+    fs.readFile(file2, "utf-8", (err, data) => {
+      if (err) {
+        return console.log(`error : ${err}`);
+      }
+      let konten2 = JSON.parse(data);
+      pesan2 = konten2[0].message;
+      tampilData[1] = pesan2.split(" ")[1];
+
+      //bacaData file3
+      fs.readFile(file3, "utf-8", (err, data) => {
+        if (err) {
+          return console.log(`error : ${err}`);
+        }
+        let konten3 = JSON.parse(data);
+        pesan3 = konten3[0].data.message;
+        tampilData[2] = pesan3.split(" ")[1];
+
+        return fnCallback(err, tampilData);
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
